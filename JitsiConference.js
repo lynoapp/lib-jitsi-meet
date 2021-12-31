6a7dc4539bc30e90c0cf6b5d8c65e82d1c0df350
Jorge Oliveira <neweracracker@gmail.com>
2021-12-31 23:12:34 +0000

    fix: better already left message

--- JitsiConference.js
+++ JitsiConference.js
@@ -669,7 +669,7 @@ JitsiConference.prototype.leave = async function() {
 
     // Leave the conference. If this.room == null we are calling second time leave().
     if (!this.room) {
-        throw new Error('The conference is has been already left');
+        throw new Error('The conference has already been left');
     }
 
     const room = this.room;
